8590beeaeabd20b0704e7689ac3dbc15cad44d79
Thavorn <thavornkhanthavong@Thavorns-MBP.lan>
2019-05-23 16:01:00 +1000

    UI Mockup Design

---
+++ eChat/eChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eChat
+//
+//  Created by Thavorn KHANTHAVONG on 23/5/19.
+//  Copyright © 2019 Thavorn KHANTHAVONG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
