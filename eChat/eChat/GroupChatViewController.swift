5e3b51cd82ee46a0fc600645011d5f42c1122175
Tribhumi <12861732@student.uts.edu.au>
2019-05-30 17:41:22 +1000

    ADD GroupChatUIViewController file

---
+++ eChat/eChat/GroupChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GroupChatViewController.swift
+//  eChat
+//
+//  Created by Tribhumi Vacharagasemsin on 30/5/19.
+//  Copyright © 2019 Thavorn KHANTHAVONG. All rights reserved.
+//
+
+import UIKit
+
+class GroupChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
